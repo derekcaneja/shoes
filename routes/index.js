3dd6d500b7607ce3cc995723291bb50a4e9a73d2
mikedemarais <m.demarais@gmail.com>
2014-08-09 12:32:54 -0400

    styles and hbs

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.color = function(req, res){
+  res.render('color', { title: 'Express' });
+};
+exports.results = function(req, res){
+  res.render('products', { title: 'Express' });
+};
